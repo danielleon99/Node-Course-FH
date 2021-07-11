8d0500a07a7b09d25af024619c46675b8ecfdd5a
Daniel Leon <danielleond99@gmail.com>
2021-07-11 19:23:29 -0400

    First commit Table of 5

---
+++ app.js
@@ -0,0 +1,9 @@
+console.clear();
+
+console.log('==============');
+console.log('Table of 5');
+console.log('==============');
+
+for (let i = 1; i < 11; i++){
+  console.log(`5 x ${i} = ${5 * i}`);  
+};
