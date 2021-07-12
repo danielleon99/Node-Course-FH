20aefc17e33cfbcfc33234925b820d6cf00c408c
Daniel Leon <danielleond99@gmail.com>
2021-07-12 11:15:14 -0400

    Add package.json

--- 03-bases-node/app.js
+++ 03-bases-node/app.js
@@ -1,13 +1,11 @@
-
 const { createFile } = require('./helpers/multiply');
 console.clear();
 
 const base = 10;
 
-
-
-
 /*
 createFile(base)
   .then( res => console.log(res))
   .catch( err => console.log(err))*/
+
+  
---
+++ 03-bases-node/outputs/info.md
@@ -0,0 +1,3 @@
+# Information
+
+This folder contains the final text files  
